6c63d0898006f6a0f9d5733cea6f24c6f879522f
Walid CHERGUI <walid.chergui@gmail.com>
2017-03-11 13:57:32 +0100

    add try test class

--- src/test/scala/part_03_functional_types/e03_Try.scala
+++ src/test/scala/part_03_functional_types/e03_Try.scala
@@ -9,7 +9,7 @@ import scala.concurrent.Future
 import scala.concurrent._
 import ExecutionContext.Implicits.global
 import scala.concurrent.duration._
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
 import models.devoxx.basic.Talk
 import models.devoxx.basic.Slot
 import models.devoxx.basic.Room
@@ -27,49 +27,12 @@ class e03_Try extends HandsOnSuite {
 
   exercice("Déclarer une Future") {
 
-    try {
-      val writer = new PrintWriter(new File("Write.txt"))
-
-      writer.write("Hello Developer, Welcome to Scala Programming.")
-      writer.close()
-    }
-    print(new File("Write.txt").getAbsolutePath)
-    Source.fromFile("Write.txt").foreach { x => print(x) }
+    Try {
+      Source.fromFile("Speaker.txt").getLines.toList
+    }.map(iter => iter.filter())
+    new File("Write.txt").getAbsolutePath shouldBe "titi"
+    .foreach { x => print(x) }
   }
 
-  object DevoxxService {
-
-    def getSlotByTalkId(talkId :String) : Future[Slot]= {
-      Future{
-        // simuler la latence du réseau
-        Thread.sleep(100)
-        Slot(id = "slot_3", room ="3",talk = talkId ,from =new DateTime(), to = new DateTime() )
-      }
-    }
-
-    def getSlot(id :String) : Future[Slot]= {
-      Future{
-        // simuler la latence du réseau
-        Thread.sleep(100)
-        Slot(id = id, room ="3",talk = "DNY-501" ,from =new DateTime(), to = new DateTime() )
-      }
-    }
-
-    def getTalk(id: String): Future[Talk] = {
-      Future{
-        // simuler la latence du réseau
-        Thread.sleep(100)
-        Talk(id, Conference, "Le bon testeur il teste...", "Pourquoi proposer une nouvelle conférence sur les tests ?", List("6cbb41adbc049f923c4327ed3642f208faf4e03f"))
-      }
-    }
-
-    def getRoomById(id: String): Option[Room] = {
-      if(id.toInt < 8 )
-        Some(Room(id,s"salle$id", Some(id.toInt * 10)))
-      else
-        None
-    }
 
   }
-
-}
