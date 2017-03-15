9aedc9d66f4c772f99fafb4abb5a55ecc0504b52
Fabrice Sznajderman <fabszn@gmail.com>
2017-03-15 12:09:04 +0100

    HOF & Currying

---
+++ src/test/scala/exercices/e05_HOF.scala
@@ -0,0 +1,36 @@
+package exercices
+
+import models.devoxx.basic.Speaker
+import support.HandsOnSuite
+
+/**
+  * Created by fsznajderman on 15/03/2017.
+  */
+class e05_HOF extends HandsOnSuite {
+
+
+  /**
+    * Dans le chapitre collection nous avons eu l'occasion de voir ce qu'était une fonction d'Ordre Supérieur.
+    * Nous allons maintenons mettre en oeuvre pour no propre besoin les proporiétés du tel fonction.
+    * Pour rappel, une fonction d'ordre supérieur est une fonction qui attend en paramètre une autre fonction
+    * et / ou qui renvoie une function en valeur de retour.
+    * L'idée est de pouvoir :
+    *   - généraliser le comportement d'une fonction,
+    *   - faire la composition de fonction
+    *
+    */
+
+  exercice("") {
+
+    /**
+      * Soit les fonctions suivantes :
+      */
+
+    val dayOne
+
+
+
+
+  }
+
+}
---
+++ src/test/scala/exercices/e06_Currying.scala
@@ -0,0 +1,10 @@
+package exercices
+
+import support.HandsOnSuite
+
+/**
+  * Created by fsznajderman on 15/03/2017.
+  */
+class e06_Currying extends HandsOnSuite{
+
+}
