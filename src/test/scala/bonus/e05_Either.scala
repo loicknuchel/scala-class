351f0c1c1419af60d592315ea12baa179e5e1d9b
Walid CHERGUI <walid.chergui@gmail.com>
2017-03-23 13:21:36 +0100

    add explanation for Either

--- src/test/scala/bonus/e05_Either.scala
+++ src/test/scala/bonus/e05_Either.scala
@@ -7,39 +7,58 @@ import support.HandsOnSuite
 
 class e05_Either extends HandsOnSuite {
 
+  /**
+    *Either[A, B] est un type, qui encapsule deux valeur de Type A et B, B (droit) représente la valeur calculée,
+    * A (gauche) représente un message d’erreur
+    * Either a deux sous-types :
+    *       Right[B] =>  si le calcul s'est déroulé avec succès
+    *       Left[A] => si l'opération a lancé une exception, A peut être un String pour un simple message d'erreur
+    *       ou une exception de type MalformedURLException, IOException ....
+    *
+    *  Example : getSpeaker a la fin de la classe, retourne une type Either[MalformedURLException, Speaker] :
+    *        si le résultat est ok => le type de retour est Right[Speaker]
+    *        si le résultat est ko => le type de retour est Left[MalformedURLException]
+
+    */
+
   // Solutions de l'exercice (en cas de besoin) : https://github.com/loicknuchel/scala-class/blob/solution/src/test/scala/bonus/e05_Either.scala
 
   exercice("Either : Right") {
-    val speaker = getSpeaker("25b6eeb75c18e3465d5cddf2be297b8863006551")
+    // récupérez le speaker 1693d28c079e6c28269b9aa86ae04a4549ad3074
+    val speaker = getSpeaker("1693d28c079e6c28269b9aa86ae04a4549ad3074")
 
+    // Sachant que le résultat de l'appel est ok, le speaker est à droit ou à gauche?
     speaker.isRight shouldBe __
     speaker.isLeft shouldBe __
 
+    //map peut être appliqué  à droit ou à gauche du Either
     val speakerName: Either[MalformedURLException, String] = speaker.right.map(speaker => speaker.firstName)
 
-    speakerName.right.get shouldBe __
+    //Depuis la variable speakerName , récupérez le nom du speaker
+    __ shouldBe "Fabrice"
   }
 
 
   exercice("Either: Left") {
     val speaker = getSpeaker("toto")
 
+    // Sachant que le speaker toto n'existe pas, le speaker est à droit ou à gauche?
     speaker.isRight shouldBe __
     speaker.isLeft shouldBe __
-
-    val speakerName = speaker.right.map(speaker => speaker.firstName).left.map(ex => ex.getMessage)
   }
 
-
+  /**
+    * on peut combiner facilement des Eithers grâce à  la fonction flatMap
+    */
   exercice("Either: Combine") {
     val speaker1 = getSpeaker("09a79f4e4592cf77e5ebf0965489e6c7ec0438cd")
     val speaker2 = getSpeaker("1693d28c079e6c28269b9aa86ae04a4549ad3074")
 
-    val sameCompany = speaker1.right.flatMap(speaker1 =>
+    val sameCompany: Either[MalformedURLException, Boolean] = speaker1.right.flatMap(speaker1 =>
       speaker2.right.map(speaker2 => speaker1.company eq speaker2.company)
     )
 
-    sameCompany.right.get shouldBe __
+    __ shouldBe false
   }
 
 
