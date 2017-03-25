8e488d779cdf092b141c3b9708cd87829c2cccc4
Loïc Knuchel <loicknuchel@gmail.com>
2017-03-25 12:23:54 +0100

    change functional types structure

---
+++ src/test/scala/bonus/e04_types_fonctionnels.scala
@@ -0,0 +1,22 @@
+package bonus
+
+import support.HandsOnSuite
+
+class e04_types_fonctionnels extends HandsOnSuite {
+  /**
+    * La vraie définition d'une Monade est :
+    *   - type `M[A]`
+    *   - `point(a: A): M[A]`
+    *   - `flatMap(f: A => M[B]): M[B]`
+    */
+
+  /**
+    * Détail for-compréhension :
+    *   - décomposition map / flatMap / filter
+    */
+
+  /**
+    * Créer une Monade Validation
+    * Utiliser la Validation dans une for-comprehension
+    */
+}
